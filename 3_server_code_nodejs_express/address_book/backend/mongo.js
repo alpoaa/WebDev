245dec085376413460f3da81e7a0d315e28b1c92
alpoaa <aleksi.e.aalto@gmail.com>
2024-12-22 08:13:08 +0200

    db from prompt

---
+++ 3_server_code_nodejs_express/address_book/backend/mongo.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Not enough parameters')
+    process.exit()
+}
+
+const dbPassword      = process.argv[2]
+const dbUrl           = `mongodb+srv://${process.env.MONGO_DB_USER}:${dbPassword}@webdev.perkp.mongodb.net/addressbook?retryWrites=true&w=majority`
+
+console.log('Trying to log into database:', dbUrl)
+mongoose.set('strictQuery', false)
+mongoose.connect(dbUrl)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = new mongoose.model('Person', personSchema)
+
+if (process.argv.length > 3) {
+    const newPersonName   = process.argv[3]
+    const newPersonNumber = process.argv[4]
+
+    const person = new Person({
+        name: newPersonName,
+        number: newPersonNumber
+    })
+
+    person.save()
+    .then(result => {
+        console.log('New person saved!')
+        mongoose.connection.close()
+    })
+} else {
+    Person.find({})
+    .then(result => {
+        result.forEach(person => {
+            console.log(person)
+        })
+        mongoose.connection.close()
+    })
+}
+
---
+++ Training/backend/mongo.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit()
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://masteruser:${password}@webdev.perkp.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'HTML is easy',
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
