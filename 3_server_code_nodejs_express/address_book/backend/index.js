2352c84386ccac2b221e9c652ee2de3230ab350c
alpoaa <aleksi.e.aalto@gmail.com>
2024-12-23 07:56:55 +0200

    address_book and database step 1 & 2

--- 3_server_code_nodejs_express/address_book/backend/index.js
+++ 3_server_code_nodejs_express/address_book/backend/index.js
@@ -1,36 +1,20 @@
+require('dotenv').config()
+
 const express = require('express')
 const morgan  = require('morgan')
+const cors    = require('cors')
 const app     = express()
+const Person  = require('./models/person')
 
 app.use(express.json())
+app.use(cors())
 morgan.token('body', (req) => {
     return req.body ? JSON.stringify(req.body) : ''
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": "1"
-    },
-    {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": "2"
-    },
-    {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": "3"
-    },
-    {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": "4"
-    }
-]
+let persons = []
 
 //GET
 app.get('/info', (req, res) => {
@@ -39,25 +23,26 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({})
+    .then(dbPersons => {
+        res.json(dbPersons)
+    })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = req.params.id
-    const person = persons.find(note => note.id === id)
-
-    if (person) {
+    Person.findById(req.params.id)
+    .then(person => {
         res.json(person)
-    } else {
+    })
+    .catch(error => {
         res.status(401).end()
-    }
-
+    })
 })
 
 //POST
 app.post('/api/persons', (req, res) => {
     const body = req.body
-  
+
     if (!body.name || !body.number) {
       return res.status(400).json({ 
         error: 'name and/or number is missing' 
@@ -72,15 +57,15 @@ app.post('/api/persons', (req, res) => {
         })
     }  
   
-    const newPerson = {
-      name: body.name,
-      number: body.number,
-      id: String(Math.floor(Math.random() * 6000) + 1)
-    }
-  
-    persons = persons.concat(newPerson)
-  
-    res.json(newPerson)
+    const newPerson = new Person({
+        name: body.name,
+        number: body.number
+    })
+
+    newPerson.save()
+    .then(savedPerson => {
+        res.json(newPerson)
+    })
 })
 
 //DELETE
@@ -91,7 +76,7 @@ app.delete('/api/persons/:id', (req, res) => {
     res.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
