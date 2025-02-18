75f5e32bc6dc0f828f273d3bbb31d9fa8def0d63
alpoaa <aleksi.e.aalto@gmail.com>
2025-02-18 07:20:04 +0200

    Training w/ redux

--- Training/redux/src/reducers/noteReducer.js
+++ Training/redux/src/reducers/noteReducer.js
@@ -13,7 +13,27 @@ const noteReducer = (state = [], action) => {
 
             return state.map(note => note.id !== id ? note : changedNote)
         default:
-            return false
+            return state
+    }
+}
+
+const generateId = () => Number((Math.random() * 100000).toFixed(0))
+
+export const createNote = (content) => {
+    return {
+        type: 'NEW_NOTE',
+        payload: {
+            content,
+            important: false,
+            id: generateId()
+        }
+    }
+}
+
+export const toggleImportanceOf = (id) => {
+    return {
+        type: 'TOGGLE_IMPORTANCE',
+        payload: { id }
     }
 }
 
